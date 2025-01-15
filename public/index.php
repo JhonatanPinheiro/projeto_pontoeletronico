cc5adcd867bd0f3064bda1b26e0b85f1c1e5b19a
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-01-15 14:58:17 -0300

    Configurações de caminhos, Criando a classe Model e  User

--- public/index.php
+++ public/index.php
@@ -2,9 +2,14 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-#Database::getConnection();
+#Criando um novo Usuário
+$user = new User(['name' => 'Lucas', 'email' => 'lucas@study.com']);
+print_r($user);
+
+/*#Database::getConnection();
 
 $sql = 'SELECT * FROM users';
 $result = Database::getResultFromQuery($sql);
@@ -12,4 +17,4 @@ $result = Database::getResultFromQuery($sql);
 while ($row = $result->fetch_assoc()) {
     print_r($row);
     echo '<br>';
-}
+}*/
