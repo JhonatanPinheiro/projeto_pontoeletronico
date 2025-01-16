b5f17c5e9b8c2b04d0f2bcc25889c03951cd319f
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-01-16 14:21:03 -0300

    Trazendo dados do banco de dados usando classe e função

--- src/models/Model.php
+++ src/models/Model.php
@@ -31,10 +31,29 @@ class Model
         $this->values[$key] = $value;
     }
 
-    public static function getSelect($filters = [], $columns = '*')
+    public static function get($filters = [], $columns = '*')
+    {
+        $objects = [];
+        $result = static::getResultSetFromSelect($filters, $columns);
+        if ($result) {
+            #Retorna o nome da classe em que um método estático foi chamado - get_called_class()
+            $class =  get_called_class();
+            while ($row = $result->fetch_assoc()) {
+                array_push($objects, new $class($row));
+            }
+        }
+        return $objects;
+    }
+
+    public static function getResultSetFromSelect($filters = [], $columns = '*')
     {
         $sql = "SELECT {$columns}  FROM " . static::$tableName . static::getFilters($filters);
-        return $sql;
+        $result = Database::getResultFromQuery($sql);
+        if ($result->num_rows === 0) {
+            return NULL;
+        } else {
+            return $result;
+        }
     }
 
     private static function getFilters($filters)
