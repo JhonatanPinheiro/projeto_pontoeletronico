ac284fdb475ae5666e4701e5b51bd707ee5c96de
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-01-23 15:23:38 -0300

    Finalizando tela de login - FrontEnd

--- src/views/login.php
+++ src/views/login.php
@@ -12,21 +12,27 @@
 </head>
 
 <body>
-    <form class="form-login" action="#" method="post">
-        <div class="login-card card bor_blu">
+    <form class="form-login border-main" action="#" method="post">
+        <div class="login-card card">
             <div class="card-header danger">
-                <span class="font-weight-light"> Ponto Eletrônico</span>
+                <span class="font-weight-light"> Ponto Eletrônico &nbsp;</span>
                 <i class="icofont-runner-alt-1"></i>
             </div>
-            <div class="card-body bor_yel">
-                <div class="form-group bor_gren">
+            <div class="card-body">
+                <div class="form-group">
                     <label for="email">E-mail</label>
                     <input type="email" id="email" name="email" class="form-control" placeholder="Informe o e-mail" autofocus>
                 </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" id="password" name="password" class="form-control" placeholder="Informe a senha">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-primary">Entrar</button>
             </div>
         </div>
     </form>
-
 </body>
 
 </html>
