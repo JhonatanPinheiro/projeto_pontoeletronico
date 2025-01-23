905652c3c23527d6678215f69c60df17405f5928
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-01-23 11:00:45 -0300

    Ajustando rotas + criação da página de login

--- src/views/login.php
+++ src/views/login.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ponto Eletrônico</title>
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+  
 </head>
 <body>
+    <div>Login</div>
     
 </body>
 </html>
