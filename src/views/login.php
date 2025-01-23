987444b04b65fba82ce6ed7347ac136c5990323d
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-01-23 12:06:26 -0300

    Criando a tela de login no frontend

--- src/views/login.php
+++ src/views/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,8 +9,8 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/icofont.min.css">
     <link rel="stylesheet" href="assets/css/login.css">
-  
 </head>
+
 <body>
     <form class="form-login" action="#" method="post">
         <div class="login-card card bor_blu">
@@ -18,10 +19,11 @@
                 <i class="icofont-runner-alt-1"></i>
             </div>
             <div class="card-body bor_yel">
-                
+
             </div>
         </div>
     </form>
-    
+
 </body>
+
 </html>
