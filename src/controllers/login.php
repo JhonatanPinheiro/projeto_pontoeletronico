cdc603a6bc1393e4b78319e6d1d1015202c797e6
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-01-24 19:01:19 -0300

    Arrumando a sintaxe do arquivo controllers/login.php

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,12 +1,12 @@
 <?php
 loadModel('Login');
 
-if(count($_POST) > 0) {
+if (count($_POST) > 0) {
     $login = new Login($_POST);
-    try{
+    try {
         $user = $login->checkLogin();
         echo "Usuário {$user->name} logado com sucesso!";
-    } catch(Exception $e){
+    } catch (Exception $e) {
         echo 'Falha no Login. Usuário ou senha Inválido';
     }
 }
