0586ba10ac38985f93243a788fa14a5d0a8c110e
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-01-24 19:32:55 -0300

    Criando mensagens de erros personalizadas utilizando o boostrap + view  com php

--- src/config/config.php
+++ src/config/config.php
@@ -6,6 +6,7 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 // Pastas do Projeto
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
+define('TEMPLATE_PATH', realpath(dirname(__FILE__) . '/../views/template'));
 define('CONTROLLER_PATH', realpath(dirname(__FILE__) . '/../controllers'));
 define('EXCEPTION_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
 
