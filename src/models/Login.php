5d66bdc7911f8a4aa7285af2a89ad578608e9a08
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-02-08 07:34:26 -0300

    Criando erros personalizados no email e senha

--- src/models/Login.php
+++ src/models/Login.php
@@ -3,8 +3,24 @@ loadModel('User');
 
 class Login extends Model
 {
+    public function validate()
+    {
+        $errors = [];
+        if (!$this->email) {
+            $errors['email'] = 'E-mail é um campo obrigatório!';
+        }
+        if (!$this->password) {
+            $errors['password'] = 'Por Favor, informe a senha!  (campo obrigatório)';
+        }
+
+        if (count($errors) > 0) {
+            throw new ValidationException($errors);
+        }
+    }
+
     public function checkLogin()
     {
+        $this->validate();
         $user = User::getOne(['email' => $this->email]);
         if ($user) {
             if ($user->end_date) {
