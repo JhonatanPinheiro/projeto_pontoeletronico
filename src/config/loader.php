8ea84a8ec910e51af3c5f714bee094bde469d645
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-02-10 03:31:44 -0300

    Criando a tela (View) do sistema batimento de ponto

--- src/config/loader.php
+++ src/config/loader.php
@@ -19,3 +19,20 @@ function loadView($viewName, $params = array())
 
     require_once(VIEW_PATH . "/{$viewName}.php");
 }
+
+function loadTemplateView($viewName, $params = array())
+{
+    if (count($params) > 0) {
+        foreach ($params as $key => $value) {
+            if (strlen($key) > 0) {
+                // Erro pode estar aqui
+                ${$key} = $value;
+            }
+        }
+    }
+
+    #require_once(VIEW_PATH . "/header.php");
+    #require_once(VIEW_PATH . "/menu.php");
+    require_once(VIEW_PATH . "/{$viewName}.php");
+    #require_once(VIEW_PATH . "/footer.php");
+}
