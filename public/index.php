c05bb90504a3653a41a180c140f8911561917448
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-02-14 18:13:25 -0300

    Ajustando url para ele pegar somento o caminho relativo

--- public/index.php
+++ public/index.php
@@ -5,9 +5,11 @@
 #Rotas de Páginas
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
-$uri = urldecode($_SERVER['REQUEST_URI']);
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-if($uri === '/' || $uri ==='' || $uri==='/index.php'){
+if ($uri === '/' || $uri === '' || $uri === '/index.php') {
     $uri = '/login.php';
 }
 
