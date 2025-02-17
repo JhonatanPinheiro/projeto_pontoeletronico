7d1448d9c804fd5bc506f5c147ea67e7b849d914
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-02-17 16:36:06 -0300

    Realizando ajuste no css do footer, header e  content + criação do nav

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -9,21 +9,34 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/icofont.min.css">
     <link rel="stylesheet" href="assets/css/template-menu.css">
-  
+
 </head>
 
 <body>
     <header class="header">
         <div class="logo">
-            <span class="font-weight-light"> Ponto Eletrônico &nbsp;</span>
+            <span class="font-weight-light mr-2"> Ponto Eletrônico &nbsp;</span>
             <i class="icofont-runner-alt-1"></i>
         </div>
-        <div class="menu-tog mx-3" style="border: 1px solid blue;">
-            <i class="icofont-navigation-menu">
+        <div class="menu-toggle mx-3">
+            <i class="icofont-navigation-menu"></i>
+        </div>
+        <div class="spacer">
 
-            </i>
-            <div class="spacer">
-
+        </div>
+        <div class="dropdown">
+            <div class="dropdown-button">
+                <span>Usuário Mock</span>
+                <i class="icofont-simple-down ml-2"></i>
+            </div>
+            <div class="dropdown-content">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="logout.php">Sair
+                            <i class="iconfont-logout mr-2"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </header>
