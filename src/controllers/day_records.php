36eb84f43ca0a177bbcb60fd98bdd6052fb7d7f6
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-02-26 14:57:31 -0300

    Visualizando os batimentos

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 requireValidSession();
-
+loadModel('WorkingHours');
 // $date = (new DateTime())->getTimestamp();
 // $today = strftime('%d de %B de %Y', $date);
 // $todayMasc = strftime('%d %m %y', $date);
@@ -9,4 +9,10 @@ requireValidSession();
 $date = new DateTime();
 $today = $date->format('d \d\e F \d\e Y'); // Formato equivalente a '%d de %B de %Y'
 
-loadTemplateView('day_records', ['today' => $today]);
+$user = $_SESSION['user'];
+$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
+
+loadTemplateView('day_records', [
+    'today' => $today,
+    'records' => $records
+]);
