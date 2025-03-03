37a333e5e9df5cc54d4317225a8500e1756312b7
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-03-03 19:36:53 -0300

    Criando tabela e populando os registros do banco de dados para o front-end + estilo do bootstrap

--- src/views/monthly_report.php
+++ src/views/monthly_report.php
@@ -1,8 +1,52 @@
 <main class="content">
     <?php
-    print_r($report);
-    echo '<hr>';
-    echo $sumOfWorkedTime;
-    echo $balance;
+    renderTitle(
+        'Relatório Mensal',
+        'Acompanhe seu saldo de horas',
+        'icofont-ui-calendar'
+    );
     ?>
+    <div>
+        <table class="table table-bordered table-striped table-hover table-primary">
+            <thead class="bg-primary text-white">
+                <th class="">Dia</th>
+                <th class="">Entrada 1</th>
+                <th class="">Saída</th>
+                <th class=""> Entrada 2</th>
+                <th class=""> Saída 2</th>
+                <th class=""> Saldo</th>
+            </thead>
+            <tbody>
+                <?php foreach ($report as $registry): ?>
+                    <tr class="">
+                        <td class="">
+                            <?= $registry->work_date  ?>
+                        </td>
+
+
+                        <td class="">
+                            <?= $registry->time1  ?>
+                        </td>
+
+                        <td class="">
+                            <?= $registry->time2  ?>
+                        </td>
+                        <td class="">
+                            <?= $registry->time3  ?>
+                        </td>
+                        <td class="">
+                            <?= $registry->time4  ?>
+                        </td>
+                        <td class="">
+                            <?= $balance ?>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+                <tr class='bg-primary text-white font-weight-bold'>
+                    <td colspan="5" class='text-center'> Horas Trabalhadas: <span><?= $sumOfWorkedTime ?></span> </td>
+                    <td colspan="1"> Saldo Mensal:  <span><?= $balance ?></span> </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </main>
