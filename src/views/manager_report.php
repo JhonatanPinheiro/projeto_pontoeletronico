6fe9a5d7e7717749f8afa86a1b9ec9105e2c1516
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2025-03-06 12:17:33 -0300

    Trabalhando na tela manager-report

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,23 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ponto Eletrônico</title>
+    <link rel="stylesheet" href="assets/css/color-personalize.css">
+    <link rel="stylesheet" href="assets/css/manager-report.css">
+</head>
+
 <main class="content">
     <?php
-    echo "Quantidade de Usuários: {$activeUsersCount}";
-    echo "Horas Trabalhadas no mês: {$hoursInMonth}";
-    print_r($absentUsers);
+    renderTitle(
+        'Relatório Gerencial',
+        'Resumo das horas trabalhadas dos funcionários',
+        'icofont-chart-histogram'
+    )
     ?>
+
+    <div class="sumamary-boxes">
+        <div class="summary-box bg-primary boder-manager-report m-1 p-2">
+            <i class="icofont-users text-white"> </i>
+            <p class="title">Quantidade de funcionários</p>
+            <h3 class="value"><?= $activeUsersCount ?></h3>
+        </div>
+    </div>
+    <div class="sumamary-boxes">
+        <div class="summary-box bg-danger boder-manager-report m-1 p-2">
+            <i class="icofont-patient-bed text-white"> </i>
+            <p class="title">Faltas</p>
+            <h3 class="value "><?= count($absentUsers) ?></h3>
+        </div>
+    </div>
+    <div class="sumamary-boxes">
+        <div class="summary-box bg-success boder-manager-report  m-1 p-2">
+            <i class="icofont-sand-clock text-white"> </i>
+            <p class="title">Horas no Mês</p>
+            <h3 class="value"><?= $hoursInMonth ?></h3>
+        </div>
+    </div>
+
+    <?php if (count($absentUsers) > 0): ?>
+        <div class="card mt-4">
+            <div class="card-header">
+                <h4 class="text-right">
+                    <span class="border-date">
+                        <?= $current_date = (new DateTime())->format('d-m-Y'); ?>
+                    </span>
+                </h4>
+                <h4 class="card-title"> Faltosos de Dia </h4>
+                <p class="card-category"> Relação dos funcionários que ainda não bateram o ponto hoje </p>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <th>Nome</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($absentUsers as $name): ?>
+                            <tr>
+                                <td>
+                                    <?= $name ?>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <?php endif ?>
 </main>
-
-
-<?php
-// session_start();
-// requireValidSession();
-
-// loadTemplateView('manager_report', [
-//     // 'report' => $report,
-//     // 'sumOfWorkedTime' => getTimeStringFromSeconds($sumOfWorkedTime),
-//     // 'balance' => "{$sign}$balance",
-//     // 'selectedPeriod' => $selectedPeriod,
-//     // 'periods' => $periods,
-//     // 'selectedUserId' => $selectedUserId,
-//     // 'users' => $users,
-
-// ]);
